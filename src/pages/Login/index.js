44af8faf0dc71f7419b5aaa687ffd55a80050135
Gabriel Assys Mendes Oliveira <110845125+GABRIEL-ASSYS@users.noreply.github.com>
2023-05-03 19:55:50 -0300

    criando hook personalizado

---
+++ src/pages/Login/index.js
@@ -0,0 +1,51 @@
+import { Button, Input, InputLabel, InputAdornment } from '@material-ui/core'
+import { Container, Titulo, InputContainer } from './styles'
+import { useHistory } from 'react-router-dom'
+import { UsuarioContext } from 'common/context/Usuario'
+import { useContext } from 'react'
+
+function Login() {
+  const history = useHistory()
+  const { nome, setNome, saldo, setSaldo } = useContext(UsuarioContext)
+  return (
+    <Container>
+      <Titulo>
+        Insira o seu nome
+      </Titulo>
+      <InputContainer>
+        <InputLabel>
+          Nome
+        </InputLabel>
+        <Input
+          value={nome}
+          onChange={(event) => setNome(event.target.value)}
+          type="text"
+        />
+      </InputContainer>
+      <InputContainer>
+        <InputLabel>
+          Saldo
+        </InputLabel>
+        <Input
+          value={saldo}
+          onChange={(event) => setSaldo(event.target.value)}
+          type="number"
+          startAdornment={
+            <InputAdornment position="start">
+              R$
+            </InputAdornment>
+          }
+        />
+      </InputContainer>
+      <Button
+        onClick={() => history.push('/feira')}
+        variant="contained"
+        color="primary"
+      >
+        Avançar
+      </Button>
+    </Container>
+  )
+};
+
+export default Login;
